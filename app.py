57e69842e202100db16dcc10255911f98a9e49ec
khto-dev <kevinhau.toronto@gmail.com>
2023-06-19 17:22:19 -0400

    doc: readme finished with video demo link

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request
 from helpers import *
 import config
 import validators
